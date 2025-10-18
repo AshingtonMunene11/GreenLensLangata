96b3f935a333644e3b8a08d1a3c7332f68aae3ec
AkumuOmolo <akumu.omoloh@gmail.com>
2025-10-18 22:47:28 +0300

    Adds development model

---
+++ server/app/models/development_plan.py
@@ -0,0 +1,32 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
+
+db = SQLAlchemy()
+
+class DevelopmentPlan(db.Model):
+    __tablename__ = 'development_plans'
+
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(50), nullable=False, unique=True)
+    description = db.Column(db.String(80), nullable = False)
+    type = db.Column(db.String(25), nullable=False)
+
+    area_size = db.Column(db.Float, nullable=False)
+    status = db.Column(db.string, default="Pending")
+
+    polygon_id=db.Column(db.String(50), nullable=False)
+    centrod_lat=db.Column(db.Float, nullable=False)
+    centrod_long=db.Column(db.Float, nullable=False)
+
+    ai_results=db.Column(db.Text(20), nullable=False)
+
+    
+
+
+
+
+
+
+
+

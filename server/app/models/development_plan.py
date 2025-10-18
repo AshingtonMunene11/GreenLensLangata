949736f0b4e2bafdb9067f2b560c13b3ee27bd78
AkumuOmolo <akumu.omoloh@gmail.com>
2025-10-18 23:49:48 +0300

    Adds model relationships with developmentplan model

--- server/app/models/development_plan.py
+++ server/app/models/development_plan.py
@@ -13,7 +13,7 @@ class DevelopmentPlan(db.Model):
     type = db.Column(db.String(25), nullable=False)
 
     area_size = db.Column(db.Float, nullable=False)
-    status = db.Column(db.string, default="Pending")
+    status = db.Column(db.__subclasshook__tring, default="Pending")
 
     polygon_id=db.Column(db.String(50), nullable=False)
     centrod_lat=db.Column(db.Float, nullable=False)
@@ -21,7 +21,28 @@ class DevelopmentPlan(db.Model):
 
     ai_results=db.Column(db.Text(20), nullable=False)
 
-    
+    # rships
+    user_id=db.Column(db.Integer, db.ForeignKey('users.id'))
+    area_id = db.relationship('Area', back_populates='development_plans', cascade= 'all, delete-orphan')
+    user_id = db.relationship('User', back_populates='development_plans', cascade= 'all, delete-orphan')
+
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'type': self.type,
+            'area_size': self.area_size,
+            'status': self.status,
+            'polygon_id': self.polygon_id,
+            'centrod_lat':self.centrod_lat,
+            'centrod_long':self.centrod_long
+        }
+
+
+
 
 
 

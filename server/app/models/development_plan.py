646a6fd234d6ab9daee2ca906689f822fd676d73
AkumuOmolo <akumu.omoloh@gmail.com>
2025-10-19 01:19:51 +0300

    Adds valiadtion to DevelopmentPlan model

--- server/app/models/development_plan.py
+++ server/app/models/development_plan.py
@@ -4,29 +4,30 @@ from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
 
+
 class DevelopmentPlan(db.Model):
     __tablename__ = 'development_plans'
 
-    id = db.Column(db.Integer,primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False, unique=True)
-    description = db.Column(db.String(80), nullable = False)
+    description = db.Column(db.String(80), nullable=False)
     type = db.Column(db.String(25), nullable=False)
 
     area_size = db.Column(db.Float, nullable=False)
     status = db.Column(db.__subclasshook__tring, default="Pending")
 
-    polygon_id=db.Column(db.String(50), nullable=False)
-    centrod_lat=db.Column(db.Float, nullable=False)
-    centrod_long=db.Column(db.Float, nullable=False)
+    polygon_id = db.Column(db.String(50), nullable=False)
+    centrod_lat = db.Column(db.Float, nullable=False)
+    centrod_long = db.Column(db.Float, nullable=False)
 
-    ai_results=db.Column(db.Text(20), nullable=False)
+    ai_results = db.Column(db.Text(20), nullable=False)
 
     # rships
-    user_id=db.Column(db.Integer, db.ForeignKey('users.id'))
-    area_id = db.relationship('Area', back_populates='development_plans', cascade= 'all, delete-orphan')
-    user_id = db.relationship('User', back_populates='development_plans', cascade= 'all, delete-orphan')
-
-
+   
+    area_id = db.relationship(
+        'Area', back_populates='development_plans', cascade='all, delete-orphan')
+    user_id = db.relationship(
+        'User', back_populates='development_plans', cascade='all, delete-orphan')
 
     def to_dict(self):
         return {
@@ -37,17 +38,18 @@ class DevelopmentPlan(db.Model):
             'area_size': self.area_size,
             'status': self.status,
             'polygon_id': self.polygon_id,
-            'centrod_lat':self.centrod_lat,
-            'centrod_long':self.centrod_long
+            'centrod_lat': self.centrod_lat,
+            'centrod_long': self.centrod_long
         }
-
-
-
-
-
-
-
-
-
-
+    
+    @validates('title')
+    def validate_name(self, key, name):
+        if not name or len(name) <1:
+            raise ValueError("Title must be at least 1 letter")
+        
+        
+        existing = DevelopmentPlan.query.filter(DevelopmentPlan.name == name.strip().first())
+        if existing and existing.id != getattr(self, "id", None):
+            raise ValueError("Station name must be unique")
+        return name.strip()
 

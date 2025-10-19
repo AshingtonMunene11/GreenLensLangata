df5b0f79877c35214060b319dcd56e2ba8b8ff3e
AkumuOmolo <akumu.omoloh@gmail.com>
2025-10-19 17:27:51 +0300

    Adds latest development model

--- server/app/models/development_plan.py
+++ server/app/models/development_plan.py
@@ -23,12 +23,12 @@ class DevelopmentPlan(db.Model, SerializerMixin):
     ai_results = db.Column(db.Text, nullable=False)
 
     # rships
-   
+
     area_id = db.relationship(
         'Area', back_populates='development_plans', cascade='all, delete-orphan')
     user_id = db.relationship(
         'User', back_populates='development_plans', cascade='all, delete-orphan')
-    
+
     serialize_rules = ('-area.station', '-user.station')
 
     def to_dict(self):
@@ -43,24 +43,16 @@ class DevelopmentPlan(db.Model, SerializerMixin):
             'centrod_lat': self.centrod_lat,
             'centrod_long': self.centrod_long
         }
-    
+
     @validates('title')
     def validate_name(self, key, title):
-        if not title or len(title) <1:
+        if not title or len(title) < 1:
             raise ValueError("Title must be at least 1 letter")
-        
-        
+
         existing = DevelopmentPlan.query.filter_by(title=title.strip()).first()
         if existing and existing.id != getattr(self, "id", None):
             raise ValueError("Development plan title must be unique")
         return title.strip()
 
-class Area(db.Model, SerializerMixin):
-    __tablename__ = 'area'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=True)
-# rships
-    development_plans = db.relationship('DevelopmentPlan', back_populates='area')
 
 

7de74bf7ec64ed851b20c5571c0f83ad889bb023
AkumuOmolo <akumu.omoloh@gmail.com>
2025-10-20 17:56:13 +0300

    Refactors development routes to include API endpoints for creating development plans

--- server/app/routes/development_routes.py
+++ server/app/routes/development_routes.py
@@ -12,7 +12,21 @@ migrate = Migrate(app, db)
 CORS(app)
 
 # my projects page
-@app.route('/api/development_plans', methods=['GET'])
+@app.route('/development_plans', methods=['GET'])
 def get_development_plans():
     plans = DevelopmentPlan.query.all()
     return jsonify([plan.to_dict() for plan in plans])
+
+@app.route('/development_plans/<int:id>', methods=['GET'])
+def get_development_plan(id):
+    plan = DevelopmentPlan.query.get_or_404(id)
+    return jsonify(plan.to_dict())
+
+@app.route('/development_plans', methods=['POST'])
+def create_plan():
+    data= request.get_json()
+    new_plan=DevelopmentPlan(title = data['title'], description=data['description'], type=data['type'], area_size =data['area_size'])
+    db.session.add(new_plan) 
+    db.session.commit()
+    return jsonify(new_plan.to_dict()), 201
+

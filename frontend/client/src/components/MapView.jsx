9afe46bc968a6dff10e78ca12f50d4438a551f44
AkumuOmolo <akumu.omoloh@gmail.com>
2025-10-22 17:23:55 +0300

    Fix polygon click handler and improve error handling in MapView component

--- frontend/client/src/components/MapView.jsx
+++ frontend/client/src/components/MapView.jsx
@@ -19,17 +19,52 @@ export default function() {
 
     const handlePolygonClick = async (polygon) => {
         try {
-            const res = await fetch (
-                `http://127.0.0.1:5000/polygons/${polygon.id}/grading`
-            );
-            const data = await res.json();
-            setselectedInsight(data);
-            catch(error) {
-                console.error("Error fetching grade", error);
-                
-            }
-        };
-    }
+        const res = await fetch(
+            `http://127.0.0.1:5000/polygons/${polygon.id}/grading`
+        );
+        const data = await res.json();
+        setSelectedInsight(data);
+        } catch (error) {
+        console.error("Error fetching grading:", error);
+        }
+    };
+
+    return (
+        <div>
+            <MapContainer
+            center={[-1.2921, 36.8219]} // Nairobi coord
+            zoom={12}
+            style ={{height: "100%", width: "100%"}}
+            >
+            
+            <TileLayer >
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution="&copy; OpenStreetMap contributors"
+
+            </TileLayer>
+
+            {polygons.map((polygon)=> {
+                const coords = JSON.parse(polygon.coordinates);
+                return (
+                    <Polygon>
+                        key={polygon.id}
+                        positions={coords}
+                        eventhandlers={{
+                            click: () =>handlePolygonClick(polygon)
+                        }}
+                        pathOptions ={{
+                            color: 
+                                selectedpolygon.id === polygon.id? orange: green, weight: 1, }}
+                    </Polygon>
+                )
+
+            })}
+
+
+            </MapContainer>
+        </div>
+    );
+
     
 
     

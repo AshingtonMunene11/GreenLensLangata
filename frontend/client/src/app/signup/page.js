f70382030925ed9e459d100aa43fe30a7b042a82
Ashington Munene <munenefrank11@gmail.com>
2025-10-22 17:57:44 +0300

    Log API URL for debugging during signup process

--- frontend/client/src/app/signup/page.js
+++ frontend/client/src/app/signup/page.js
@@ -25,6 +25,7 @@ export default function Signup() {
 
     try {
       // const res = await fetch("http://127.0.0.1:5000/api/auth/register"
+      console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
       const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },

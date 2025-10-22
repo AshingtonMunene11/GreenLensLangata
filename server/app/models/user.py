44e77da5cc63a9573cd098d81540b076bed2acf1
Ashington Munene <munenefrank11@gmail.com>
2025-10-22 14:07:56 +0300

    Implement user authentication with registration and login routes; add user model and database migration setup

---
+++ server/app/models/user.py
@@ -0,0 +1,27 @@
+from ..extensions import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from email_validator import validate_email, EmailNotValidError
+from sqlalchemy.orm import validates
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+
+    @validates('email')
+    def validate_email(self, key, address):
+        try:
+            valid = validate_email(address)
+            return valid.email
+        except EmailNotValidError as e:
+            raise ValueError("Please enter a valid email address")
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f"<User {self.username}, {self.email}>"

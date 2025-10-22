3cba08aa96e87ea1a63b2f8358ae2cd966edd15c
AkumuOmolo <akumu.omoloh@gmail.com>
2025-10-22 17:54:41 +0300

    Add Developmentpage component to render MapView and h1

---
+++ frontend/client/src/app/development/page.js
@@ -0,0 +1,21 @@
+
+
+import React from "react";
+import MapView from "@/components/MapView";
+// import { MapContainer } from "react-leaflet";
+
+function Developmentpage() {
+  return (
+    <div>
+        <h1>
+            Plan your next project responsibly.
+        </h1>
+        <p>
+
+        </p>
+      <MapView />
+    </div>
+  );
+}
+
+export default page;

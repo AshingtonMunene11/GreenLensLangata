e6dfbf3d23291c26d1b6585987dd6ff8ddc0e387
AkumuOmolo <akumu.omoloh@gmail.com>
2025-10-24 13:03:39 +0300

    Refactors route registration in create_app function

--- server/app/routes/__init__.py
+++ server/app/routes/__init__.py
@@ -1,28 +1,29 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from app.routes.polygon_routes import register_routes
 
-db = SQLAlchemy()  
+db = SQLAlchemy()
+
 
 def create_app():
     app = Flask(__name__)
 
-   
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
- 
+
     db.init_app(app)
 
     from app.models import DevelopmentPlan, Area, Polygon, AIInsights
-  
-    from app.routes import development_routes, explore_routes, polygon_routes, langata_insights
+
+    from app.routes import development_routes, explore_routes, polygon_routes, langata_insights_routes
 
     # Register your new routes
-    langata_insights.register_langata_routes(app)
+    langata_insights_routes.register_langata_routes(app)
 
-   
     with app.app_context():
         db.create_all()
 
-    return app
+    register_routes(app)
 
+    return app

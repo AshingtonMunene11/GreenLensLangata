73aea8126df54457d57621f30333978528128ecf
Brian Cheruiyot <briancheru11@gmail.com>
2025-10-27 05:20:24 +0300

    Update community import routes

--- server/app/routes/__init__.py
+++ server/app/routes/__init__.py
@@ -4,10 +4,6 @@ from .polygon_routes import register_routes as register_polygon_routes
 from .langata_insights_routes import register_langata_routes
 from .community_routes import register_community_routes
 from .auth_routes import auth
-from .community_routes import register_community_routes
 # from app.routes.chat_routes import chat_bp
 # from .chat import chat_bp
-from .chat_routes import chat_bp
-
-
-
+# from .chat_routes import chat_bp

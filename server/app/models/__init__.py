e56ea6303a2183da4524e1b7ff41b8ffc55b60a4
Brian Cheruiyot <briancheru11@gmail.com>
2025-10-27 05:17:52 +0300

    Add user in models/init.py

--- server/app/models/__init__.py
+++ server/app/models/__init__.py
@@ -5,7 +5,7 @@ from .development_plan import DevelopmentPlan
 from .polygon import Polygon
 from .report import Report
 from .ai_insight import AIInsights
-# from .user import User  # Uncomment when ready
+from .user import User  # Uncomment when ready
 
 __all__ = [
     "db",
@@ -13,27 +13,6 @@ __all__ = [
     "DevelopmentPlan",
     "Polygon",
     "Report",
-    "AIInsights"
+    "AIInsights",
+    "User"
 ]
-
-# <<<<<<< Akumu
-# from app import db
-
-# from .area import Area
-# from .development_plan import DevelopmentPlan
-# from .polygon import Polygon
-
-# # from .report import Report
-# from .ai_insight import AIInsights
-# # from .user import User
-
-
-# __all__ = ["AIInsights", "DevelopmentPlan", "Area", "Polygon"]
-
-# =======
-# from app.extensions import db
-# from app.models.report import Report
-
-
-# __all__ = ["db", "Report"]
-# >>>>>>> Development

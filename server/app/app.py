fad37a44f6fd163c746ae0ee5da564f76b754ad3
AkumuOmolo <akumu.omoloh@gmail.com>
2025-10-28 22:19:57 +0300

    Refactors app initialization with CORS

--- server/app/app.py
+++ server/app/app.py
@@ -1,15 +1,18 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_migrate import Migrate
-from models import db, DevelopmentPlan, Area, AIinsights, Polygon
+from app.models import db, DevelopmentPlan, Area, Polygon, PolygonAnalysis, Insights
 from config import Config
 from flask_cors import CORS
+from app.routes.GEE_Polygon_Analysis_routes import gee_bp
+import os
 
+app = Flask(__name__)
+CORS(app)
 
-app=Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
-migrate=Migrate(app, db)
+migrate = Migrate(app, db)
 
-CORS(app)
 
+app.register_blueprint(gee_bp, url_prefix="/gee")

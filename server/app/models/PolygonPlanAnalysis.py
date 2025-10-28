ca342dded5f4b876e3257f56eaf2a800765ebe9a
AkumuOmolo <akumu.omoloh@gmail.com>
2025-10-28 11:47:46 +0300

    Adds user id column for relationship with User model

--- server/app/models/PolygonPlanAnalysis.py
+++ server/app/models/PolygonPlanAnalysis.py
@@ -10,8 +10,6 @@ class PolygonAnalysis(db.Model, SerializerMixin):
 
     development_plan_id = db.Column(db.Integer, db.ForeignKey(
         "development_plans.id"), nullable=False)
-    development_plan = db.relationship(
-        "DevelopmentPlan", backref=db.backref("analyses", lazy=True))
 
     polygon_id = db.Column(db.Integer, db.ForeignKey(
         "polygons.id"), nullable=False)
@@ -31,33 +29,24 @@ class PolygonAnalysis(db.Model, SerializerMixin):
     polygon = db.relationship(
         "Polygon", backref=db.backref("analyses", lazy=True))
 
-    serialize_rules = ("-development_plan", "-Polygon",)
+    development_plan = db.relationship(
+        "DevelopmentPlan", back_populates="analysis")
 
     # ---USER R/SHIP --
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    user = db.relationship("User", backref=db.backref("analyses", lazy=True))
 
-    # user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    # user = db.relationship("User", backref=db.backref("analyses", lazy=True))
-
-    def calculate_and_update_plan_status(self):
-        """
-        Automatically calculates flora loss % and updates associated DevelopmentPlan status.
-        """
-        plan = self.development_plan
-        if self.flora_area and plan.area_size:
-            flora_loss_pct = (plan.area_size / self.flora_area) * 100
-            plan.status = "Pass" if flora_loss_pct <= 5 else "Fail"
-        else:
-            plan.status = "Unknown"
-        db.session.commit()
+    serialize_rules = ("-development_plan", "-polygon", "-user",)
 
     def to_dict(self):
         return {
             "id": self.id,
             "polygon_id": self.polygon_id,
+            "development_plan_id": self.development_plan_id,
             "built_up_area": self.built_up_area,
             "flora_area": self.flora_area,
             "built_up_pct": self.built_up_pct,
             "flora_pct": self.flora_pct,
-
+            "user_id": self.user_id,
             "created_at": self.created_at.isoformat()
         }

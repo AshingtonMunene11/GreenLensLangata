f7bebce3b1d33cb1df6db41c740b9e06a7ea231b
Ashington Munene <munenefrank11@gmail.com>
2025-10-29 11:57:42 +0300

    updated the config to have relative path for static folder

--- server/app/config.py
+++ server/app/config.py
@@ -7,9 +7,13 @@ load_dotenv()
 
 class Config:
     # Base paths
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    INSTANCE_DIR = os.path.join(BASE_DIR, '..', 'instance')
-    UPLOAD_FOLDER = os.path.join(BASE_DIR, '..', 'static', 'uploads')
+    BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    INSTANCE_DIR = os.path.join(BASE_DIR, 'instance')
+    UPLOAD_FOLDER = os.path.join(BASE_DIR, 'app', 'static', 'uploads')
+    
+    # BASE_DIR = os.path.abspath(os.path.dirname(__file__)) 
+    # INSTANCE_DIR = os.path.join(BASE_DIR, '..', 'instance')
+    # UPLOAD_FOLDER = os.path.join(BASE_DIR, '..', 'static', 'uploads')
 
     # Database
     SQLALCHEMY_DATABASE_URI = os.getenv(

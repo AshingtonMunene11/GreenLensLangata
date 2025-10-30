80acd9d1a7ac2110b775f996ce2772b33c4c18f5
Ashington Munene <munenefrank11@gmail.com>
2025-10-30 08:46:26 +0300

    modified config

--- server/app/config.py
+++ server/app/config.py
@@ -16,11 +16,19 @@ class Config:
     # UPLOAD_FOLDER = os.path.join(BASE_DIR, '..', 'static', 'uploads')
 
     # Database
-    SQLALCHEMY_DATABASE_URI = os.getenv("postgresql://greenlens_db2_user:coM6K760W43QzIZ1kEVmm7bQctjYa3J8@dpg-d41ek4jipnbc73fanj4g-a.oregon-postgres.render.com/greenlens_db2",
-        "DATABASE_URI",
-        'sqlite:///' + os.path.join(INSTANCE_DIR, 'greenlens.db')
-    )
+    # SQLALCHEMY_DATABASE_URI = os.getenv("postgresql://greenlens_db2_user:coM6K760W43QzIZ1kEVmm7bQctjYa3J8@dpg-d41ek4jipnbc73fanj4g-a.oregon-postgres.render.com/greenlens_db2",
+    #     "DATABASE_URI",
+    #     'sqlite:///' + os.path.join(INSTANCE_DIR, 'greenlens.db')
+    # )
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    _database_url = os.environ.get("DATABASE_URL")
+    if _database_url and _database_url.startswith("postgres://"):
+        _database_url = _database_url.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = _database_url or f"sqlite:///{os.path.join(INSTANCE_DIR, 'greenlens.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
     # Security
     SECRET_KEY = os.getenv("SECRET_KEY", "dev_secret")
